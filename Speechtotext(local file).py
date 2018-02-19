76c09c5e5ba6401a7b077992e1a2817948682f72
varun95 <varun96505@gmail.com>
2018-02-19 12:01:31 +0530

    Add files via upload
    Only .raw extension and 64bitencoded files working

---
+++ Speechtotext(local file).py
@@ -0,0 +1,42 @@
+import io
+import os
+import base64
+
+
+
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="C:/Users/Varun/Desktop/Sign Language Translator-87ce12c513f9.json"
+print(os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
+
+client = speech.SpeechClient()
+
+
+def encode_audio(audio):
+ audio_content = audio.read()
+ return base64.b64encode(audio_content)
+
+
+file="C:/Users/Varun/Desktop/New folder (7)/audio2.raw"
+
+
+
+with io.open(file, 'rb') as audio_file:
+  content = audio_file.read()
+
+
+audio = types.RecognitionAudio(content=content)
+config = types.RecognitionConfig(
+    encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+    sample_rate_hertz=16000,
+    language_code='en-US')
+
+response = client.recognize(config, audio)
+# Each result is for a consecutive portion of the audio. Iterate through
+# them to get the transcripts for the entire audio file.
+for result in response.results:
+    # The first alternative is the most likely one for this portion.
+    print('Transcript: {}'.format(result.alternatives[0].transcript))
+
