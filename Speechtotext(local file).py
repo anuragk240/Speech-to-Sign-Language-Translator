5cbe89c43205773fa8253342752ce9e30b050272
anuragk240 <anuragk240@gmail.com>
2018-02-24 14:06:18 +0530

    Reformatted code. Added comments.

--- Speechtotext(local file).py
+++ Speechtotext(local file).py
@@ -2,41 +2,74 @@ import io
 import os
 import base64
 
-
-
 from google.cloud import speech
 from google.cloud.speech import enums
 from google.cloud.speech import types
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="D:\\anurag\Audio to sign language translator\Sign Language Translator-c72c78bbb40e.json"
-print(os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
-
-client = speech.SpeechClient()
-
-
+# not necessary as for now
 def encode_audio(audio):
- audio_content = audio.read()
- return base64.b64encode(audio_content)
+    audio_content = audio.read()
+    return base64.b64encode(audio_content)
 
+def speech_to_text():
+    from record import RATE
+    from record import WAVE_OUTPUT_FILENAME
+    from record import FLAC_OUTPUT_FILENAME
+    from record import PATH_TO_AUDIO_FILE
 
-file="C:/Users/Varun/Desktop/New folder (7)/audio2.raw"
+    # location to the json key file. Replace with your location
+    json_key_file = "D:\\anurag\Audio to sign language translator\Sign Language Translator-c72c78bbb40e.json"
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = json_key_file
+    print(os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
 
+    client = speech.SpeechClient()
 
+    # if this script is run from blender, specify the path of audio file that is to be converted into flac
+    # PATH_TO_AUDIO_FILE points to current directory. Audio file is saved in that path.
+    path = PATH_TO_AUDIO_FILE
+    print(path)
+    input_file = path + '\\' + WAVE_OUTPUT_FILENAME
+    output_file = path + '\\' + FLAC_OUTPUT_FILENAME
 
-with io.open(file, 'rb') as audio_file:
-  content = audio_file.read()
+    # another approach for converting wav to flac
+    """
+    conversion_command = 'ffmpeg -ac 1 -i' + input_file + ' ' + output_file
+    import subprocess
+    p = subprocess.Popen(conversion_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p_out, p_err = p.communicate()
+    """
 
+    # convert wav to flac. Be sure that ffmpeg is installed and added to environmental
+    # variable path. Restart pycharm after installing.If not working than execute script from cmd.
+    try:
+        from pydub import AudioSegment
+        song = AudioSegment.from_wav(input_file)
+        song.export(output_file, format="flac")
+    except:
+        print("Conversion Failed.")
+        print("Make sure ffmpeg is installed and added to environment variable.")
+        print("And try running script from cmd instead of pycharm.")
+        return
 
-audio = types.RecognitionAudio(content=content)
-config = types.RecognitionConfig(
-    encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
-    sample_rate_hertz=16000,
-    language_code='en-US')
+    # read audio file
+    with io.open(output_file, 'rb') as audio_file:
+        content = audio_file.read()
 
-response = client.recognize(config, audio)
-# Each result is for a consecutive portion of the audio. Iterate through
-# them to get the transcripts for the entire audio file.
-for result in response.results:
-    # The first alternative is the most likely one for this portion.
-    print('Transcript: {}'.format(result.alternatives[0].transcript))
+    # number of channel in audio should be 1 for both wav file and flac file
+    audio = types.RecognitionAudio(content=content)
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
+        sample_rate_hertz=RATE,
+        language_code='en-US')
 
+    response = client.recognize(config, audio)
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+    for result in response.results:
+        # The first alternative is the most likely one for this portion.
+        print('Transcript: {}'.format(result.alternatives[0].transcript))
+    print("Finished")
+
+# executes this code if script is executed directly in cmd or pycharm
+if __name__ == "__main__":
+    speech_to_text()
--- record.py
+++ record.py
@@ -1,48 +1,62 @@
 import pyaudio
+import os
 import wave
 
 FORMAT = pyaudio.paInt16
-CHANNELS = 2
+
+# Keep number of channel one to avoid errors in speech api like retry error
+CHANNELS = 1
 RATE = 44100
 CHUNK = 1024
 RECORD_SECONDS = 10
 WAVE_OUTPUT_FILENAME = "file.wav"
+FLAC_OUTPUT_FILENAME = 'file.flac'
+PATH_TO_AUDIO_FILE = os.path.dirname(os.path.abspath(__file__))
 
-audio = pyaudio.PyAudio()
+def record_audio():
+    audio = pyaudio.PyAudio()
 
-# starts Recording
-stream = audio.open(format=FORMAT, channels=CHANNELS,
-                    rate=RATE, input=True,
-                    frames_per_buffer=CHUNK)
-print "Please say something......"
-frames = []
+    # starts Recording
+    stream = audio.open(format=FORMAT, channels=CHANNELS,
+                        rate=RATE, input=True,
+                        frames_per_buffer=CHUNK)
+    print("Please say something......")
+    frames = []
 
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    frames.append(data)
-print "Thank you for Input"
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+    print("Thank you for Input")
 
-# stop Recording
-stream.stop_stream()
-stream.close()
-audio.terminate()
+    # stop Recording
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
 
-waveFile = wave.open('C:/Users/Varun/Desktop/New folder (7)/file.wav', 'wb')
-waveFile.setnchannels(CHANNELS)
-waveFile.setsampwidth(audio.get_sample_size(FORMAT))
-waveFile.setframerate(RATE)
-waveFile.writeframes(b''.join(frames))
-waveFile.close()
+# get current directory and save recorded file in that directory
+    path = PATH_TO_AUDIO_FILE
+    path = path + '\\' + WAVE_OUTPUT_FILENAME
+    print(path)
+    waveFile = wave.open(path, 'wb')
+    waveFile.setnchannels(CHANNELS)
+    waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+    waveFile.setframerate(RATE)
+    waveFile.writeframes(b''.join(frames))
+    waveFile.close()
 
-play=pyaudio.PyAudio()
-stream_play=play.open(format=FORMAT,
-                      channels=CHANNELS,
-                      rate=RATE,
-                      output=True)
+    play = pyaudio.PyAudio()
+    stream_play = play.open(format=FORMAT,
+                            channels=CHANNELS,
+                            rate=RATE,
+                            output=True)
 
-#Gives Output of recording
-for data in frames:
-    stream_play.write(data)
-stream_play.stop_stream()
-stream_play.close()
-play.terminate()
+    # Gives Output of recording
+    for data in frames:
+        stream_play.write(data)
+    stream_play.stop_stream()
+    stream_play.close()
+    play.terminate()
+
+# executes this code if script is executed directly in cmd or pycharm
+if __name__ == "__main__":
+    record_audio()
