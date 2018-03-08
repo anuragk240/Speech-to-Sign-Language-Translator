0ff61fff20319a77d64dd6531a36659ea124af3f
varun95 <varun96505@gmail.com>
2018-03-09 00:03:25 +0530

    Add files via upload
    NLP Technique applied on text received from an audio recording, in hope of getting important word/phrases from the speech which would be given as an input to the animation model.

---
+++ NLP.py
@@ -0,0 +1,26 @@
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="C:/Users/Varun/Desktop/Sign Language Translator-87ce12c513f9.json"
+print(os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
+
+
+
+# Instantiates a client
+client = language.LanguageServiceClient()
+
+# The text to analyze
+text = 'Hello, world!'
+document = types.Document(
+    content=text,
+    type=enums.Document.Type.PLAIN_TEXT)
+
+# Detects the sentiment of the text
+sentiment = client.analyze_sentiment(document=document).document_sentiment
+
+print('Text: {}'.format(text))
+print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
